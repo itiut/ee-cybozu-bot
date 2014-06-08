d27fbf2e6a8462f5b3a3383528487577127d5015
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-06-08 19:33:16 +0900

    Use DATABASE_URL to connect database

--- db/connection.rb
+++ db/connection.rb
@@ -1,6 +1,3 @@
-require 'erb'
 require 'sequel'
-require 'yaml'
 
-config = YAML.load(ERB.new(File.read(File.expand_path('../../config/database.yml', __FILE__))).result)[ENV['HEROKU_ENV']]
-DB = Sequel.connect(config)
+DB = Sequel.connect(ENV['DATABASE_URL'])
