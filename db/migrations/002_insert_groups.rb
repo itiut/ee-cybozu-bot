f2e35aea66652f3d9f14879405c19f055df72566
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-06-08 20:30:34 +0900

    Change group.id according to actual gid

--- db/migrations/002_insert_groups.rb
+++ db/migrations/002_insert_groups.rb
@@ -1,8 +1,8 @@
 Sequel.migration do
   up do
-    self[:groups].insert(id: 1, name: '全体')
-    self[:groups].insert(id: 2, name: '学部学生')
-    self[:groups].insert(id: 3, name: '全大学院生')
+    self[:groups].insert(id: 0, name: '全体')
+    self[:groups].insert(id: 2574, name: '全大学院生')
+    self[:groups].insert(id: 2576, name: '学部学生')
   end
 
   down do
